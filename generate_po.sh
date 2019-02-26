5ae28f4e35b209f9746407a0dd582b822cd00b1c
juanma1980 <juanma1980@gmail.com>
2019-02-26 17:39:21 +0100

    :x

--- generate_po.sh
+++ generate_po.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-PYTHON_FILES="../scheduler-gui.install/usr/share/taskscheduler/bin/taskScheduler.py ../python3-taskscheduler.install/usr/share/taskscheduler/*.py ../scheduler-gui.install/usr/share/taskscheduler/bin/detailDateBox.py"
-CMD_DIR="../files.install/etc/scheduler/conf.d/commands"
-TASKS_DIR="../files.install/etc/scheduler/conf.d/tasks"
+PYTHON_FILES="scheduler-gui.install/usr/share/taskscheduler/bin/taskScheduler.py python3-taskscheduler.install/usr/share/taskscheduler/*.py scheduler-gui.install/usr/share/taskscheduler/bin/detailDateBox.py"
+CMD_DIR="files.install/etc/scheduler/conf.d/commands"
+TASKS_DIR="files.install/etc/scheduler/conf.d/tasks"
 
-mkdir -p taskscheduler/
+mkdir -p translations/
 
-xgettext $PYTHON_FILES -o taskscheduler/taskscheduler.pot
+xgettext $PYTHON_FILES -o translations/taskscheduler.pot
+
+echo "" >> translations/taskscheduler.pot
 
 DIR=$PWD
 cd $CMD_DIR
@@ -15,13 +17,23 @@ do
 	awk -v wrkdir=$DIR -v filename=$CMD_DIR"/"$i '{
 	if ($0~":")
 	{ 
-		outfile=wrkdir"/taskscheduler/taskscheduler.pot"
+		outfile=wrkdir"/translations/taskscheduler.pot"
 		printf ("#: %s:%s\n",filename,NR) >> outfile
 		split($0,a,":")
-		gsub(/\"/,"",a[1])
-		gsub(/\t*/,"",a[1])
-		printf ("msgid \"%s\"\n",a[1]) >> outfile
-		printf("msgstr \"\"\n\n") >> outfile
+		if ($0!~"cmd\":")
+		{
+			if ($0!~"parms\":")
+			{
+				gsub(/\"/,"",a[1])
+				gsub(/\t*/,"",a[1])
+				printf ("msgid \"%s\"\n",a[1]) >> outfile
+			} else {
+				gsub(/\"/,"",a[2])
+				gsub(/\t*/,"",a[2])
+				printf ("msgid \"%s\"\n",a[2]) >> outfile
+			}
+			printf("msgstr \"\"\n\n") >> outfile
+		}
 
 	}
 	}' $i
@@ -33,7 +45,7 @@ do
 	awk -v wrkdir=$DIR  -v filename=$TASKS_DIR"/"$i '{
 	if ($0~":")
 	{ 
-		outfile=wrkdir"/taskscheduler/taskscheduler.pot"
+		outfile=wrkdir"/translations/taskscheduler.pot"
 		printf ("#: %s:%s\n",filename,NR) >> outfile
 		split($0,a,":")
 		gsub(/\"/,"",a[1])
